38ba5b8636b5d54ce3b54a353418e3cf6706b439
mawad@utexas.edu <mawad@utexas.edu>
2017-01-02 11:04:41 +0000

    first push after holidays

---
+++ headers.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h> // needed to use socklent_t (32 bit)
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>         //needed for memset
+#include <iostream>
+
--- modbusServer.cpp
+++ modbusServer.cpp
@@ -1,13 +1,5 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h> // needed to use socklent_t (32 bit)
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>         //needed for memset
-#include <iostream>
+#include "headers.h"
 
 #define BUFFER_SIZE 260
 #define PORT_502 502
